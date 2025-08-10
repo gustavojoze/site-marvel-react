62253b3a04ee50e6b5e77c319cc313754fda8a34
gustavojoze <gustavojosesoaressantana@gmail.com>
2025-08-10 05:18:14 -0300

    style: estiliza o component Cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,20 +2,32 @@ import styled from 'styled-components';
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { closeCart } from '../redux/cartSlice';
+import ItenCart from './ItenCart';
 
 const Aside = styled.aside`
   position: fixed;
   top: 0;
   right: ${({ open }) => (open ? '0' : '-300px')}; /* animação lateral */
   width: 300px;
-  height: 100%;
+  height: 100vh;
   background: #20232a;
   color: white;
-  padding: 1rem;
   box-shadow: -2px 0 5px rgba(0,0,0,0.3);
   transition: right 0.3s ease;
   z-index: 1000;
   overflow-y: auto;
+  positon: realative;
+`;
+
+const ContainerItens = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: #333539ff;
+  height: 75%;
+  overflow-y: auto;
+  gap: 1rem;
+  padding: 1rem;
+  margin-bottom: 1rem;
 `;
 
 const Cart = ( ) => {
@@ -25,18 +37,23 @@ const Cart = ( ) => {
 
   return (
     <Aside open={isOpen}>
-      <h2>Carrinho</h2>
-      <button onClick={ () => dispatch(closeCart())} style={{ marginBottom: '1rem' }}>Fechar</button>
-        <div >
-          <p>fds</p>
-        </div>
+      <svg onClick={()=>{dispatch(closeCart())}} data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width={27} height={25} style={{ cursor: 'pointer', position:"absolute", top:"10px", right:"10px" }}>
+        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"></path>
+      </svg>
+      <h2 style={{margin:"10px", fontWeight:"200", marginTop:"30px"}}>Carrinho</h2>
+      <ContainerItens>
+        <ItenCart />
+        <ItenCart />
+      </ContainerItens>
+      <h3 style={{fontWeight:"200", margin:"10px"}}>Preço total: R$423</h3>
       <input
         value={input}
         onChange={(e) => setInput(e.target.value)}
         placeholder="Digite o cupom"
+        style={{marginLeft: '10px' }}
       />
-      <button >Aplicar cupom</button>
-      <p>Cupom aplicado:fdsfds</p>
+      <button style={{marginLeft: "10px", padding: "5px 10px", backgroundColor: "#4CAF50", color: "white", border: "none", borderRadius: "5px" }} >Aplicar</button>
+      <p  style={{marginLeft: '10px'}}>Cupom aplicado:fdsfds</p>
     </Aside>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,6 +13,7 @@ const Nav = styled.nav`
     background: #20232a;
     color: white;
     margin-bottom: 20px;
+    
     `;
 
 const Logo = styled.img`
@@ -20,14 +21,7 @@ const Logo = styled.img`
     height: 40px;
     `;
 
-const StyledLink = styled(Link)`
-    color: white;
-    text-decoration: none;
-    font-weight: bold;
-    &:hover {
-        text-decoration: underline;
-    }
-    `;
+
 
 const Navbar = () =>{ 
     const dispatch = useDispatch();
@@ -38,7 +32,9 @@ const Navbar = () =>{
         <Link to="/">
             <Logo src="/logo_marvel.png" alt="Marvel Logo" />
         </Link>
-        <button onClick={() =>  dispatch(toggleCart())}>Carrinho</button>
+        <svg data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" onClick={() => dispatch(toggleCart())} style={{ cursor: 'pointer', width: '40px', height: '40px' }}>
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"></path>
+        </svg>
     </Nav>
     <Cart/>
     </>
